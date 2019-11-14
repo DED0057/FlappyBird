2f3ed8ba260cc548d6b2c5b9f809fd6fbd68bd0e
DED0057 <matyas648@gmail.com>
2019-11-14 15:13:24 +0100

    #ADD base classes and layout

---
+++ app/src/main/java/com/example/flappybird/Game.java
@@ -0,0 +1,4 @@
+package com.example.flappybird;
+
+class Game {
+}
--- app/src/main/java/com/example/flappybird/MainActivity.java
+++ app/src/main/java/com/example/flappybird/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.flappybird;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void startGame(View view){
+        Intent myIntent = new Intent(this, Game.class);
+
     }
 }
